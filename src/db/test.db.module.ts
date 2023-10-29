603e8479c09a7cab0ee5ece265a98349510b1a5c
Turgay Gulmez <turgay.gulmez@medirect.com.mt>
2023-10-29 14:07:29 +0300

    remove unused files

--- src/db/test.db.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DatabaseProvider } from '../providers/test.db.provider';
-
-@Module({
-  providers: [...DatabaseProvider],
-  exports: [...DatabaseProvider],
-})
-export class TestDatabaseModule {}
